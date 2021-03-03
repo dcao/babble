93da70bed4213041699e19ffe1d325458439e3bf
David Cao <david@cao.sh>
2021-03-02 18:04:21 -0800

    add more testing things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use babble::smiley_lang::*;
+use clap::Clap;
+use std::fs;
+
+/// Experimental library learning.
+#[derive(Clap)]
+#[clap(version = "0.1.0", author = "David Cao <david@cao.sh>")]
+struct Opts {
+    input: String,
+}
+
 fn main() {
-    println!("babble 0.1.0");
+    let opts: Opts = Opts::parse();
+    let f = opts.input;
+
+    let content = fs::read(&f).expect(&format!("error: failed to open slide file {}", &f));
+    let input = String::from_utf8_lossy(&content);
+
+    let expr = run_single(&input);
+
+    println!("output:");
+    println!("{}", expr);
 }
--- src/smiley_lang.rs
+++ src/smiley_lang.rs
@@ -1,25 +1,29 @@
 //! The AST defining the smiley language.
 
-use egg::{define_language, Id};
+use egg::{rewrite as rw, *};
 use ordered_float::NotNan;
 
+pub type EGraph = egg::EGraph<Smiley, ()>;
+pub type Rewrite = egg::Rewrite<Smiley, ()>;
 pub type Constant = NotNan<f64>;
 
 define_language! {
     /// A smiley expression
-    pub enum Expr {
+    pub enum Smiley {
+        // Meta:
         "let" = Let([Id; 3]),
-        "fn" = Lambda([Id; 2]),
+        "fn" = Fn([Id; 2]),
+        "app" = App([Id; 2]),
         "var" = Var(Id),
-        
-        "circle" = Circle([Id; 2]),
-        "segment" = Segment([Id; 3]),
-        "translate" = Translate([Id; 2]),
+
+        // Shapes:
+        "circle" = Circle,
+        "line" = Segment([Id; 3]),
+
+        "move" = Move([Id; 3]),
         "scale" = Scale([Id; 2]),
         "rotate" = Rotate([Id; 2]),
         "+" = Compose([Id; 2]),
-
-        "pos" = Position([Id; 2]),
 
         Signed(i32),
         Float(Constant),
@@ -27,3 +31,64 @@ define_language! {
     }
 }
 
+fn test_anti_unif_rules() -> Vec<Rewrite> {
+    vec![
+        rw!(
+            "example";
+            "(let f (fn x x) (let ?v1 (app f (circle ?r1 ?pos1)) (let ?v2 (app f (circle ?r2 ?pos2)) ?b)))" =>
+            // Absorbing the circle requires absorbing the required arguments to circle
+            "(let f (fn r (fn pos (circle r pos))) (let ?v1 (app (app f ?r1) ?pos1) (let ?v2 (app (app f ?r2) ?pos2) ?b)))"),
+    ]
+}
+
+// Rules for testing if an egg var can bind an expr and its arguments
+// e.g. can ?b bind "move 4 2"
+fn test_binder_rules() -> Vec<Rewrite> {
+    vec![
+        rw!("test_binder"; "(rotate ?t (?b (scale ?v ?s)))" => "(rotate ?t (fn s (?b (scale s ?s))))"),
+    ]
+}
+
+struct ToyCostFn;
+
+impl CostFunction<Smiley> for ToyCostFn {
+    type Cost = isize;
+
+    fn cost<C>(&mut self, enode: &Smiley, mut costs: C) -> Self::Cost
+    where
+        C: FnMut(Id) -> Self::Cost,
+    {
+        let op_cost = match enode {
+            Smiley::Fn(_) => -100,
+            _ => 1,
+        };
+        enode.fold(op_cost, |sum, id| sum + costs(id))
+    }
+}
+
+/// Execute EGraph building and program extraction on a single expression
+/// containing all of the programs to extract common fragments out of.
+///
+/// Programs should be in the form:
+/// ```
+/// let e1 = (...)
+/// let e2 = (...)
+/// let e3 = (...)
+/// ```
+pub fn run_single(expr: &str) -> RecExpr<Smiley> {
+    // Our list of rewrite rules is here
+    let rules: &[Rewrite] = &test_binder_rules();
+
+    // First, parse the expression and build an egraph from it
+    let expr = expr.parse().unwrap();
+    let runner = Runner::default().with_expr(&expr).run(rules);
+    let (egraph, root) = (runner.egraph, runner.roots[0]);
+
+    // Then, extract the best program from the egraph, starting at
+    // the root
+    let mut extractor = Extractor::new(&egraph, ToyCostFn);
+    let (_best_cost, best) = extractor.find_best(root);
+
+    // Return the best program
+    best
+}
