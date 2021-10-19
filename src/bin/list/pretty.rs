f84dbbb72b6d0fbf5c276249f61caed060490816
Nadia Polikarpova <nadia.polikarpova@gmail.com>
2021-10-19 12:37:32 -0700

    Refactor printer state into a struct

--- src/bin/list/pretty.rs
+++ src/bin/list/pretty.rs
@@ -1,55 +1,62 @@
 use crate::lang::ListOp;
 use babble::ast_node::Expr;
 
-pub fn pretty(expr: &Expr<ListOp>) -> String {
-  let mut buf: String = String::new();
-  print(&mut buf, expr, &mut vec![]);
-  buf
+pub fn pretty(expr: &Expr<ListOp>) -> String {  
+  let mut printer = Printer {buf : String::new(), bindings : vec![]};
+  printer.print(expr);
+  printer.buf
 }
 
-fn print(buf: &mut String, expr: &Expr<ListOp>, bindings: &mut Vec<String>) {
-  match (expr.0.operation(), expr.0.args()) {
-    (&ListOp::Int(i), []) => buf.push_str(&format!("{}", i)),
-    (&ListOp::Bool(b), []) => buf.push_str(&format!("{}", b)),
-    (&ListOp::Index(i), []) => buf.push_str(&bindings[bindings.len() - i.0 - 1]),
-    (&ListOp::Ident(ident), []) => {
-      let name: &str = ident.into();
-      if name == "empty" {
-        buf.push_str("[]");
-      } else {
-        buf.push_str(ident.into());
+struct Printer {
+  buf: String,
+  bindings: Vec<String>
+}
+
+impl Printer {
+  fn print(&mut self, expr: &Expr<ListOp>) {
+    match (expr.0.operation(), expr.0.args()) {
+      (&ListOp::Int(i), []) => self.buf.push_str(&format!("{}", i)),
+      (&ListOp::Bool(b), []) => self.buf.push_str(&format!("{}", b)),
+      (&ListOp::Index(i), []) => self.buf.push_str(&self.bindings[self.bindings.len() - i.0 - 1]),
+      (&ListOp::Ident(ident), []) => {
+        let name: &str = ident.into();
+        if name == "empty" {
+          self.buf.push_str("[]");
+        } else {
+          self.buf.push_str(ident.into());
+        }
+      },
+      (&ListOp::Cons, [head, tail]) => {
+        self.print(head);
+        self.buf.push_str(" : ");
+        self.print(tail);
+      },
+      (&ListOp::Apply, [fun, arg]) => {
+        self.print(fun);
+        self.buf.push(' ');
+        self.print(arg);
+      },
+      (&ListOp::Lambda, [body]) => {
+        let fresh_name = format!("x{}", self.bindings.len());
+        self.buf.push_str(&format!("λ{} -> ", &fresh_name));
+        self.bindings.push(fresh_name);      
+        self.print(body);
+      },
+      (&ListOp::Lib, [name, def, body]) => {
+        self.buf.push_str("lib ");
+        self.print(name);
+        self.buf.push_str(" = ");
+        self.print(def);
+        self.buf.push_str(" in\n");
+        self.print(body);
       }
-    },
-    (&ListOp::Cons, [head, tail]) => {
-      print(buf, head, bindings);
-      buf.push_str(" : ");
-      print(buf, tail, bindings);
-    },
-    (&ListOp::Apply, [fun, arg]) => {
-      print(buf, fun, bindings);
-      buf.push(' ');
-      print(buf, arg, bindings);
-    },
-    (&ListOp::Lambda, [body]) => {
-      let fresh_name = format!("x{}", bindings.len());
-      buf.push_str(&format!("\\{} -> ", &fresh_name));
-      bindings.push(fresh_name);      
-      print(buf, body, bindings);
-    },
-    (&ListOp::Lib, [name, def, body]) => {
-      buf.push_str("lib ");
-      print(buf, name, bindings);
-      buf.push_str(" = ");
-      print(buf, def, bindings);
-      buf.push_str(" in\n");
-      print(buf, body, bindings);
+      (&ListOp::List, ts) => {
+        for t in ts {
+          self.print(t);
+          self.buf.push('\n');
+        }
+      },
+      _ => self.buf.push_str("expression!")
     }
-    (&ListOp::List, ts) => {
-      for t in ts {
-        print(buf, t, bindings);
-        buf.push('\n');
-      }
-    },
-    _ => buf.push_str("expression!")
   }
 }
